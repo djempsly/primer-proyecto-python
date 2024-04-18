2faf1f14dd44335e0be164af7977f27b9fd29a1c
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-04-18 11:29:40 -0400

    Modificando el archivo main y agregando un nuevo archivo

--- main/main.py
+++ main/main.py
@@ -1,2 +1,3 @@
 print ("Hola")
 print("En este programa voy a tratar de hacer todo lo posible para poder aprender inteligencia artificial")
+#EN esta parte coy a trabajar con variables
---
+++ vars.py
@@ -0,0 +1,15 @@
+print("En esta parte veremos las variables")
+
+mi_nombre = "Jeffry"
+
+mi_edad = 34
+
+print(mi_nombre + "y ", mi_edad)
+
+# Usando input para introducir informaciones a programa, es como si fuera un prompt
+
+my_name = input("¿Cuál es tu nombre")
+
+print("mi nombre es", my_name)
+
+
