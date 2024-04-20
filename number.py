204da1748740d337dcfbc6954cbada7f2e6337bb
Jeffry Almonte <djempsly120@gmail.com>
2024-04-20 17:38:12 -0400

    Agregando componentes de los numeros al proyecto

---
+++ number.py
@@ -0,0 +1,14 @@
+
+mes1 = 450
+mes2 = 550
+mes3 = 640
+resultado = mes1 + mes2 + mes3
+
+promedio = (resultado * 3) / 100
+
+print(promedio)
+
+
+
+
+
---
+++ string_1.py
@@ -0,0 +1,29 @@
+#trabajando con String,d e manera que podamos entender lo que es cadena de texto
+string = "Hola"
+print(string)
+
+nombre = "Jeffry"
+apellido = "Almonte Dely"
+print(nombre)
+print(apellido)
+
+nombre_completo = nombre + apellido
+
+print(nombre_completo)
+
+#fotmato
+
+template = "Hola mi nombre es {} y  mi apellido es {}".format(nombre, apellido)
+
+print (template)
+
+template2 = "En te apartado {nombre} y hacer lo otro {apellido}"
+
+print(template2)
+
+
+
+
+
+
+
