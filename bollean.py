721a21756e850c81190fa76685fb857da86b0537
Jeffry Almonte <djempsly120@gmail.com>
2024-04-22 09:50:55 -0400

    Agregando el archivo boolean y cambioDatoTipo

---
+++ bollean.py
@@ -0,0 +1,6 @@
+Married1 = True
+Married2 = False
+
+print(not True
+      
+      )
---
+++ cambioDatoTipo.py
@@ -0,0 +1,16 @@
+numero = 52
+
+cambioNumero = str(input("escribe tu edad"))
+
+print('tu edad es' +  cambioNumero)
+
+nick_name = "Maline"
+name = "Djempsly"
+apellido = "Volmar"
+edad = "33"
+
+total = int(edad) + 10
+
+datos = "Hola mi apodo es {}, mi verdadero nombre es {}, mi apellido es {}, mi edad es {}".format(nick_name, name, apellido, edad)
+
+print (datos)
