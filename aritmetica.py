b78f48646d1a13bc791e36e43dd0f970c4570f70
Jeffry Almonte <djempsly120@gmail.com>
2024-04-22 13:38:33 -0400

    Agregando los archivos Aritmetica y operadores

---
+++ aritmetica.py
@@ -0,0 +1,36 @@
+# en est apartado vamos a trabajar con los operadores
+# >
+
+print(41>40)
+print(40>41)
+print(41>41)
+
+print("Esto es para trabajar con los tipos de datos mayores")
+
+#Ahora con el signo menor que <
+
+print(25 <26)
+print(26<25)
+print(26<26)
+
+# (==) este es el operador igual
+# != es te es el operador de negacion
+# <= este es el operador de menor que
+# >= est es el operador de mayor que
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operadores.py
@@ -0,0 +1,24 @@
+print(10 + 10)
+print(20 - 15)
+print(20 / 4)
+print(8 * 5)
+print(10 % 2)
+print(10 // 3)
+print(10 / 3)
+print("Hola" * 3)
+print("Hola" + "Hola")
+
+print(5**8)
+
+
+
+
+
+
+
+
+
+
+
+
+
