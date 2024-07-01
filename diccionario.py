a64e24f460a2e8cc1a42c2f491be6bd30f85bbf2
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-07-01 06:34:58 -0400

    Agregando las clases de condicionales

--- diccionario.py
+++ diccionario.py
@@ -4,9 +4,41 @@
 diccionary = {
     'Yeudy': 'Mi primer hijo',
     'Ismael': 'Segundo hijo',
-    'Jeff': 'Tercer hijo'
+    'Jeff': 'Tercer hijo',
+    'hijos': ['uno', 'dos', 'tres']
 }
+
+alfa = {
+    'Color': 'Rubia',
+    'Altura': 'Alta',
+    'Tamaño' : 'Grande',
+    'age': 30
+}
+
+
+busca = input('Escribe las palabras ')
+busca = busca.lower()
 
 print(diccionary['Yeudy'])
 
 print(diccionary.get('Ismael'))
+print(len(diccionary))
+
+if busca in alfa:
+    print('Tu nombre es Alfanelis Liberato')
+
+diccionary['Jeff'] = 'Jeffry' #Aqui es para modificar datos en un diccionario
+alfa['age'] += 4
+diccionary['hijos'].append('cinco')
+
+
+print(diccionary)
+print(alfa)
+
+diccionary.pop('Yeudy')
+del diccionary['Jeff']
+print(diccionary)
+alfa['twitter'] = 'Djempsly'
+print(alfa)
+print(list(alfa.keys()))
+print(list(alfa.values()))
---
+++ while.py
@@ -0,0 +1,11 @@
+# En esta clase vamos a 
+
+if True:
+    print('Este es mi hijo')
+
+hijos =['yeudy', 'Ismael', 'Jeff' ]
+
+while len(hijos):
+    print('SOn mis hijos')
+    if hijos == 10:
+        break
